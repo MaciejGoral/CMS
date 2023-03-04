7b710b51dbb5eb0bba6bcecf897c2bdffd5186ae
Maciej Goral <maciej636107@gmail.com>
2023-03-04 19:05:17 +0100

    Docker

--- Backend/cms_api/cms_api/settings.py
+++ Backend/cms_api/cms_api/settings.py
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'cms_api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'cms',
-        'USER': 'postgres',
-        'PASSWORD': '636107',
-        'HOST': 'localhost',
+        'USER': 'user',
+        'PASSWORD': 'pass',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
